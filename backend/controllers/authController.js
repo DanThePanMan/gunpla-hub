110ceb0f5ccb3fd1c573c18c73c15c2417a7061c
DanThePanMan <danielchenschw@gmail.com>
2025-09-03 14:21:51 -0400

    Update routes

---
+++ backend/controllers/authController.js
@@ -0,0 +1,8 @@
+async function loginGet(req, res) {
+    const creds = req.params;
+
+    console.log(creds);
+}
+module.exports = {
+    loginGet,
+};
---
+++ backend/routes/authRouter.js
@@ -0,0 +1,6 @@
+const { Router } = require("express");
+const authController = require("../controllers/authController");
+
+const authRouter = Router();
+
+authRouter.get("/login", authController.loginGet);
