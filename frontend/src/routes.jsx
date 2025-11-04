d972d469ce6b32f91b27ddabb04553c7e3dc374b
DanThePanMan <danielchenschw@gmail.com>
2025-11-04 16:21:23 -0500

    fix routes

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,24 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 
-//pages
 import Login from "./pages/Login.jsx";
 import Welcome from "./pages/Welcome.jsx";
 import SignUp from "./pages/SignUp.jsx";
 import Home from "./pages/Home.jsx";
-import Post from "./pages/post.jsx";
+import Post from "./pages/Post.jsx";
 import User from "./pages/User.jsx";
 import Layout from "./components/universal/Layout.jsx";
 
 const router = createBrowserRouter([
     { path: "/", element: <Welcome /> },
-    { path: "Login", element: <Login /> },
-    { path: "Signup", element: <SignUp /> },
+    { path: "/login", element: <Login /> },
+    { path: "/signup", element: <SignUp /> },
     {
-        // Routes that should show the sidebar are children of Layout
         path: "/",
         element: <Layout />,
         children: [
-            { path: "Home", element: <Home /> },
+            { path: "home", element: <Home /> },
             { path: "post/:id", element: <Post /> },
             { path: "user/:id", element: <User /> },
         ],
